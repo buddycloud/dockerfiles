ba0ba5e08df02b8c1d768600967e3e2445102fa4
Abmar Barros <abmargb@gmail.com>
2014-08-23 15:29:31 -0300

    added api config plus a bunch of fixes on apache2 startup

---
+++ buddycloud-stack/config/buddycloud-http-api-config.js
@@ -0,0 +1,18 @@
+// config.js:
+// HTTP API server configuration. See README.md for details.
+
+// Default settings (apply to all profiles)
+exports._ = {
+  port: 9123,
+};
+
+// Production settings (used by default)
+exports.production = {
+  debug: true,
+  xmppDomain: 'buddycloud.dev',
+  xmppHost: '127.0.0.1',
+  xmppAnonymousDomain: 'anon.buddycloud.dev',
+  channelDomain: 'buddycloud.buddycloud.dev',
+  searchComponent: 'search.buddycloud.org',
+  homeMediaRoot: 'http://localhost:60080',
+};
